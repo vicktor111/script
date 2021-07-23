41652bf61276c1067730c5eb142d1111c6582304
vicktor111 <85438133+vicktor111@users.noreply.github.com>
2021-07-23 10:45:21 +0300

    Виправив код

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import sys
 import time
-# import math
 sys.path.append("C:\\Users\\user\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages")
 import bpy
 import keyboard
@@ -8,7 +7,7 @@ import keyboard
 
 print("Start.")
 bpy.app.handlers.frame_change_pre.clear()
-dict={
+dict = {
     90: 1.5707963267948966,
     180: 3.141592653589793,
     0: 0.0,
@@ -19,6 +18,7 @@ dict={
 class Object:
 
     def __init__(self, speed: float = 0.5):
+        bpy.data.objects[bpy.context.object.name].rotation_euler[2] = 0.0
         self.r = 0.0
         self.speed = speed
         self.location = bpy.data.objects[bpy.context.object.name].location
@@ -27,44 +27,42 @@ class Object:
     def w(self):
         if dict[0] == self.rotation[2]:
             translate(self.location[0], self.location[1] + self.speed)
-        if dict[90] == self.rotation[2]:
-            translate(self.location[0] + self.speed, self.location[1])
-        if dict[180] == self.rotation[2]:
-            translate(self.location[0], self.location[1] - self.speed)
-        if dict[-90] == self.rotation[2]:
+        if self.rotation[2] == 1.5707963705062866:
             translate(self.location[0] - self.speed, self.location[1])
+        if self.rotation[2] == 3.1415927410125732 or self.rotation[2] == -3.1415927410125732:
+            translate(self.location[0], self.location[1] - self.speed)
+        if self.rotation[2] == -1.5707963705062866:
+            translate(self.location[0] + self.speed, self.location[1])
 
     def s(self):
         if dict[0] == self.rotation[2]:
             translate(self.location[0], self.location[1] - self.speed)
-        if dict[90] == self.rotation[2]:
-            translate(self.location[0] - self.speed, self.location[1])
-        if dict[180] == self.rotation[2]:
+        if self.rotation[2] == 1.5707963705062866:
+            translate(self.location[0] + self.speed, self.location[1])
+        if self.rotation[2] == 3.1415927410125732 or self.rotation[2] == -3.1415927410125732:
             translate(self.location[0], self.location[1] + self.speed)
-        if dict[-90] == self.rotation[2]:
-            translate(self.location + self.speed, self.location[1])
+        if self.rotation[2] == -1.5707963705062866: 
+            translate(self.location[0] - self.speed, self.location[1])
 
     def a(self):
         time.sleep(0.5)
         self.r += dict[90]
-        #print(self.r * 180 / math.pi)
         if self.r == 4.71238898038469:
-            #print("1")
             self.rotation[2] = dict[-90]
+            self.r = dict[-90]
         else:
             self.rotation[2] = self.r
         
     def d(self):
         time.sleep(0.5)
         self.r -= dict[90]
-        #print(self.r * 180 / math.pi)
         if self.r == -4.71238898038469:
-            #print("2")
             self.rotation[2] = dict[90]
+            self.r = dict[90]
         else:
             self.rotation[2] = self.r
 
-mesh=Object(2.0)
+mesh = Object(2.0)
 
 def translate(x, y):
     bpy.data.objects[bpy.context.object.name].location[0] = x
